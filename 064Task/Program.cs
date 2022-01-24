1005ec0a7602b5dd4c45016c613b04ba22701069
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-24 20:52:41 +0300

    Программа выдает равнобедренный треугольник символов состоящий только из 1

---
+++ 064Task/Program.cs
@@ -0,0 +1,37 @@
+﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+void showPaskalTriangle(int[,] array)
+{
+    int spaceNumbers = array.GetLength(0);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int k = 0; k < spaceNumbers;k++)
+        {
+            Console.Write(" ");
+        }
+        for(int j = 0; j <= i; j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        spaceNumbers--;
+        Console.WriteLine();
+    }
+}
+int length = 5;
+int[,] paskalTriangle = new int[length,length];
+
+for(int i = 0; i < length; i++)
+{
+    for(int j = 0 ; j < length; j++)
+    {
+        if(i == j || i == 0)
+        {
+            paskalTriangle[i,j] = 1;
+        }
+        else if (i>j)
+        {
+            paskalTriangle[i,j] = paskalTriangle[i,j] + paskalTriangle[i-1,j];
+        }
+    }
+}
+showPaskalTriangle(paskalTriangle);
