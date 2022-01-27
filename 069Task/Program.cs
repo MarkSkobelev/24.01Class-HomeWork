b5c8d962f740f8ac60c6e1a0a3aca5c1ab9ddab5
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-27 21:41:25 +0300

    Программа возвращает сумму элементов от M до N

---
+++ 069Task/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumOfNumbers(int userM, int userN)
+{
+    if(userM<=userN)
+    {
+        return userM + SumOfNumbers(userM+1,userN);   
+    }
+    return 0;
+}
+int result = SumOfNumbers(1, 5);
+Console.WriteLine(result);
