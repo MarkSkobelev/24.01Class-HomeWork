b719529285ae31349bdb1274abc635f5759ab5cb
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-27 21:26:24 +0300

    Выполнил первое задание по рекурсии

---
+++ 066Task/Program.cs
@@ -0,0 +1,12 @@
+﻿//Показать натуральные числа от 1 до N, N задано
+
+void ShowNumbers(int userNumbers, int index)
+{
+    if(index <= userNumbers)
+    {
+        Console.WriteLine(index);
+        ShowNumbers(userNumbers, index+1);
+    }
+}
+int N = 10;
+ShowNumbers(N, 5);
