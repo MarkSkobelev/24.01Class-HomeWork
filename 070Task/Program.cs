25c72172a34d1bb0c4fccada5db3f63217b2da74
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-27 21:52:06 +0300

    Программа считает сумму цифр в числе

---
+++ 070Task/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму цифр числа
+
+int SumOfDigit(int number)
+{
+    if(number>=1)
+    {
+        return number%10 + SumOfDigit(number/10);
+    }
+    return 0;
+}
+int number = SumOfDigit(893);
+Console.WriteLine(number);
