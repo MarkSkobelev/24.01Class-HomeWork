0dca20ded5fe2f25dd53959bb1ac7457458faacc
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-28 19:36:18 +0300

    Программа возводит число в указанную степень, используя метод рекурсии. Можно доработать ввод чисел с клавиатуры, и попробовать убрать 3 аргумент у рекурсивной функции.

---
+++ 072Task/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую степень B
+
+int PowMyNumber(int A, int B, int index)
+{
+    if(index<B)
+    {
+        Console.WriteLine("Программа зашла в цикл в {0} раз", index+1);
+        index++;
+        return A * PowMyNumber(A,B,index);
+    }
+    else return 1;
+}
+Console.WriteLine(PowMyNumber(5,3,0));
