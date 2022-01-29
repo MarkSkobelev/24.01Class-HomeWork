796af734c2b8b8f4233efeb0e5e5c2aa0a906f17
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-29 16:55:35 +0300

    Программа должна выдавать на экран слова из заданного алфавита. Пока не получается

---
+++ 074Task/Program.cs
@@ -0,0 +1,29 @@
+﻿// В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+
+void showWords(string str, int number, int indexForStr, int indexForNumber)
+{
+    if(indexForNumber<number)
+    {
+        if(indexForStr<str.Length)
+        {
+            Console.Write(str[indexForStr]);
+            showWords(str, number, indexForStr, indexForNumber+1);
+        }
+        Console.WriteLine();
+        Console.Write(indexForNumber);
+        if(indexForNumber==number-1)
+        {
+            if(indexForStr<str.Length)
+            {
+                Console.Write(str[indexForStr]);
+                showWords(str,number,indexForStr+1,indexForNumber);
+            }
+        }
+    }
+}
+string alphabet = "аисв";
+Console.WriteLine("Укажите длину слов, которые нужно составить");
+int number = Convert.ToInt32(Console.ReadLine());
+showWords(alphabet, number, 0, 0);
+
+
