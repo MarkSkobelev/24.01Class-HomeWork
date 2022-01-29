2339c563f712b8cd1d180c2b21829074828f002a
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-29 13:06:27 +0300

    Программа выводит на экран натуральные числа от N до 1

---
+++ 067Task/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+void ShowNumbers(int number)
+{
+    if(number>=1)
+    {
+        Console.Write($"{number} ");
+        ShowNumbers(number-1);
+    }
+}
+
+ShowNumbers(10);
