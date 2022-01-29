057f360199a42570420280d1773239b6b31b6ec2
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-29 13:27:24 +0300

    Программа выдает на экран числа от M до N, при этом оба эти числа заданы

---
+++ 068Task/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void ShowNumbers(int M, int N)
+{
+    if(M<=N)
+    {
+        Console.Write($"{M} ");
+        ShowNumbers(M+1,N);
+    }
+}
+ShowNumbers(3,16);
