2bf455efa920907917d748b4ac284e099a60691c
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-30 11:32:29 +0300

    Программа реализует результат работы функции аккермана. При первом значении равном 4, а втором - 3 переполняется стэк. Скорей всего из-за того, что эта функция быстрорастущая.

---
+++ 071Task/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу вычисления функции Аккермана
+
+double funcAkkerman(double m, double n)
+{
+    if(m==0)
+    {
+        return n+1;
+    }
+    else if(n==0)
+        {
+            return funcAkkerman(m-1,1);
+        }
+        else 
+        {   
+            return funcAkkerman(m-1,funcAkkerman(m,n-1));
+        }
+}
+
+Console.WriteLine(funcAkkerman(4,3));
